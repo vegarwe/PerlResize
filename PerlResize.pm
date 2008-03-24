d1f13c05cc6f1742490a6a044a483b6048bbf298
root <root@ygdrasil>
2008-03-24 22:35:59 +0100

    Hmmm, merkelig commit. NÃ¥ funker iallefall alt. Noen TODO'er igjen i PerlResize::Register. Den kan jeg nemlig ingenting om...

--- PerlResize.pm
+++ PerlResize.pm
@@ -3,7 +3,7 @@ package PerlResize;
 #
 # Script that resize and cache images based on input.
 #
-# e.g. http://media-host/resize.pl/<widgth>x<height>/path/img.jpg
+# e.g. http://media.host/path/img.jpg?geometry=<width>x<height>
 #
 # Author: Vegar Westerlund <vegarwe@gmail.com>
 #
@@ -12,41 +12,40 @@ package PerlResize;
 
 use strict;
 use warnings;
-use Image::Magick;
-use CGI::Carp qw(fatalsToBrowser);
-use POSIX qw();
-use MIME::Types;
-use File::stat;
-use IO::File;
 use HTTP::Date;
+use APR::Table;
+use File::stat;
+use Image::Magick;
 use File::Basename qw(fileparse);
+use Apache2::Const -compile => qw(:common HTTP_NOT_MODIFIED HTTP_METHOD_NOT_ALLOWED);
 
 our $max_size = 5000;
-our $cache = "/var/www/testlab.abakus.ntnu.no/cache";
+our $cache = "/var/www/abakus.no/app/files/cache/";
 
 sub handler {
 	my $r = shift; 
 
-	my ($file, $args, $cfile, $stat, $cstat);
-
-	$r->log_error("PerlResize");
-	$r->log_error("  content_type: ".$r->content_type());
-	$r->log_error("  filename:     ".$r->filename);
-	$r->log_error("  path_info:    ".$r->path_info);
-	$r->log_error("  args:         ".$r->args);
-	$r->log_error("  finfo         ".$r->finfo);
+	#$r->log_error("PerlResize");
+	#$r->log_error("  content_type: ".$r->content_type());
+	#$r->log_error("  filename:     ".$r->filename);
+	#$r->log_error("  path_info:    ".$r->path_info);
+	#$r->log_error("  args:         ".$r->args);
+	#$r->log_error("  finfo         ".$r->finfo);
 
 	# Must be image, with cusom geometry and read permissions must be granted
 	return Apache2::Const::DECLINED unless $r->content_type() =~ m#^image/.*$#;
     	return Apache2::Const::DECLINED unless $r->args;
 	return Apache2::Const::DECLINED unless -r $r->filename;
 
+	my ($file, $args, $cfile, $stat, $cstat, $modified_since);
+
 	$file = $r->filename;
 	$r->args =~ m#geometry=(\d+)x(\d+)#;
 	if ($1 > $max_size || $2 > $max_size) {
 		# TODO: Send '405 Method Not Allowed', "Size $1x$2 out of max range"
   		$r->log_error("Size ($1x$2) out of max range");
-  		return Apache2::Const::SERVER_ERROR;
+		$r->custom_response(Apache2::Const::HTTP_METHOD_NOT_ALLOWED, "<h1>405 Method Not Allowed</h1><p>Size ($1x$2) out of max range</p>");
+  		return Apache2::Const::HTTP_METHOD_NOT_ALLOWED;
 	}
 	$args = "$1x$2";
 	my ($name,$path,$suffix) = File::Basename::fileparse($file, '\.\w+');
@@ -56,33 +55,29 @@ sub handler {
 
 	$stat = File::stat::stat($file);
 
-	# TODO: Fix $ENV
-	#$r->log_error("ENV: " . $ENV);
-	#my $modified_since = HTTP::Date::str2time($ENV{HTTP_IF_MODIFIED_SINCE});
-	#if (defined $modified_since && $modified_since > $stat->mtime) {
-	#	$r->log_error("not modified");
-	#	#print "Status: 304 Not Modified\n\n" ;
-        #	return Apache2::Const::DECLINED;
-	#}
+	$modified_since = $r->headers_in->get('If-Modified-Since');
+	$modified_since = HTTP::Date::str2time($modified_since);
+	if (defined $modified_since && $modified_since >= $stat->mtime) {
+		$r->log_error("not modified");
+        	return Apache2::Const::HTTP_NOT_MODIFIED;
+	}
 
 	$cstat = File::stat::stat($cfile);
 
 	if (defined $cstat && defined $stat && $cstat->mtime > $stat->mtime) {
 		$r->log_error("cache hit");
+		$r->mtime($stat->mtime);
+		$r->headers_out->set('Last-Modified' => HTTP::Date::time2str($stat->mtime));
 		$r->sendfile($cfile);
         	return Apache2::Const::OK;
 	}
 
-	$r->log_error("cache miss");
-
 	my $q = Image::Magick->new;
 	my $err = $q->Read($file);
 	if ($q->Get('Colorspace') eq "CMYK") {
-		$q->Set(colorspace=>'RGB');
+		$err ||= $q->Set(colorspace=>'RGB');
 	}
 
-	#my (%arguments) = $r->args;
-	#$err ||= $q->Resize(%arguments);
 	$err ||= $q->Resize(geometry => $args);
 	$err ||= $q->Strip();
 	$err ||= $q->Write(filename => $cfile);
@@ -92,6 +87,9 @@ sub handler {
     	    return Apache2::Const::SERVER_ERROR;
     	}
 
+	$r->log_error("cache miss");
+	$r->mtime($stat->mtime);
+	$r->headers_out->set('Last-Modified' => HTTP::Date::time2str($stat->mtime));
 	$r->sendfile($cfile);
         return Apache2::Const::OK;
 }
--- startup.pl
+++ startup.pl
@@ -1,15 +1,18 @@
 # Standard mod_perl startup.pl -- load most common required modules
 
 use Bundle::Apache2 ();
-use lib qw(
-        /etc/apache2/perl
-);
 
-use Apache::DBI;
-
-# use ModPerl::Util (); #for CORE::GLOBAL::exit
+# TODO: These should be fixed with 'use Bundle::Apache2 ();', but it doesn't
+use Apache2::Log;
+use Apache2::Response ();
+use Apache2::RequestIO ();
+use Apache2::RequestRec ();
 
 use Apache2::Const -compile => ':common';
 use APR::Const -compile => ':common';
+
+use lib qw(
+        /etc/apache2/perl
+);
 
 1;
